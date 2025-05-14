dc00cbe23319a80be1e45ababc5f8a42d65ef6c3
kimjunyo <junn4580@gmail.com>
2025-05-14 15:38:52 +0900

    Initial commit

---
+++ src/main/java/com/ssafy/bjh/OnlineTrainingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTrainingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTrainingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/bjh/OnlineTrainingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.bjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTrainingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
